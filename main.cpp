2e5a7d9469d71fa643e72b0430412275bf4a63dc
BigBlackBob-93 <101857203+BigBlackBob-93@users.noreply.github.com>
2022-12-01 22:37:50 +0700

    you should use "getline()" with strings

--- main.cpp
+++ main.cpp
@@ -4,11 +4,15 @@ int main()
     // enter the code
     string code;
     cout << "Enter your code: ";
-    cin >> code;
+    getline(cin, code);
     Lexer t(code);
 
     // submit the code for lexical analysis -> you will have a vector of tokens
     vector<Token>List = t.Analysis();
+    for(int i =0; i < List.size(); i++){
+        cout << endl;
+        List[i].GetInfo();
+    }
 
     //you can use token methods to get information about it
     // example: List[0].getType(); List[0].getToken();
